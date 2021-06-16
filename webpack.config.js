bd6dda42d174ee51dcd8439921c50b2070e49101
bondcoder <andruha3097@gmail.com>
2021-06-17 00:14:26 +0300

    new: gitignore

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const webpack = require("webpack");
+const vue_loader = require("vue-loader/lib/plugin");
+const mode = (process.env.NODE_ENV === "production" ? "production" : "development"); 
+
+const config = {
+     entry:{
+        app:  "./src/app.js"
+     },
+     output:{
+         path: path.join(__dirname, "dist"),
+         filename: "[name].js"
+     },
+     module:{
+         rules: [
+           {test: /\.vue$/, use:"vue-loader"},
+           {test: /\.css$/, use:["vue-style-loader","css-loader"]},
+           {
+            test: /\.scss$/,
+            use: [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          },
+          {
+          test: /\.(png|jpe?g|gif)$/i,
+          loader: "file-loader",
+          options: {
+            name: './img/[name].[ext]',
+          }
+         }
+        ]
+     },
+     mode,
+     plugins: [new webpack.NoEmitOnErrorsPlugin(),
+               new vue_loader()],
+    resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+     }
+    }
+
+}
+
+module.exports = config;
